bd40d80a11ffc1bc3d4d962093ab58de74f3e7c7
milenb17 <milenb17@gmail.com>
2022-10-05 16:07:58 -0400

    Modified to deploy to heroku

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ var app = express();
 
 //set up mongoose connection
 const mongoose = require("mongoose");
-const mongoDB = "mongodb+srv://mongomb:mongomb@cluster0.5yfvvkp.mongodb.net/local_library?retryWrites=true&w=majority";
+const dev_db_url = "mongodb+srv://mongomb:mongomb@cluster0.5yfvvkp.mongodb.net/local_library?retryWrites=true&w=majority"; 
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
